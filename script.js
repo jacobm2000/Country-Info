923441e0f75268e15e069b9bb88342f7b306bae0
jacobm2000 <maurerja2000@gmail.com>
2024-03-12 15:08:27 -0400

    Added Map Functionality
    When a country is show to the user a google maps frame pops up with the country on a map

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 var newBtn=document.getElementById("getNew")
+var map=document.getElementById("map")
 function getCountry(word) {
     var word=document.getElementById("word")
     var pageNum=Math.floor(Math.random() *6)+1
-    console.log(pageNum)
+
     fetch("https://api.worldbank.org/v2/countries/?format=json&page="+pageNum).then(function(resp) {
-        console.log(resp);
+        
         return resp.json()
     }).then(function(data) {
         l=data[1].length
         x=Math.floor(Math.random() *l)
-        console.log(data[1][x].region.value)
+        lat=data[1][x].latitude
+        lon=data[1][x].longitude
+        map.src="https://maps.google.com/maps?q="+lat+","+lon+"&hl=en&z=2&amp&output=embed"
         if(data[1][x].region.value ==="Aggregates"){
             getCountry()
+            
         }
        word.textContent=data[1][x].name;
         
